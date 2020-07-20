223fbfbc9076f2302bce2454050441862a0d995a
Lakshay Bhambri <lakshay.bhambri@go-jek.com>
2020-07-20 15:57:00 +0530

    Minor Fixes

---
+++ sample-grpc-server/project.clj
@@ -0,0 +1,36 @@
+(defproject sample-grpc-server "0.0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Apache License 2.0"
+            :url "https://www.apache.org/licenses/LICENSE-2.0"
+            :year 2019
+            :key "apache-2.0"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [io.pedestal/pedestal.service "0.5.7"]
+
+                 ;; -- PROTOC-GEN-CLOJURE --
+                 [protojure "1.2.0"]
+                 [protojure/google.protobuf "0.9.1"]
+
+                 ;; -- PROTOC_GEN_CLOJURE CLIENT DEP --
+                 [org.eclipse.jetty.http2/http2-client "9.4.25.v20191220"]
+
+                 ;; Include Undertow for supporting HTTP/2 for GRPCs
+                 [io.undertow/undertow-core "2.0.28.Final"]
+                 [io.undertow/undertow-servlet "2.0.28.Final"]
+                 ;; And of course, protobufs
+                 [com.google.protobuf/protobuf-java "3.11.1"]
+                 ;; logging
+                 [com.taoensso/timbre "4.10.0"]
+                 [com.fzakaria/slf4j-timbre "0.3.17"]
+
+                 [ch.qos.logback/logback-classic "1.2.3" :exclusions [org.slf4j/slf4j-api]]
+                 [org.slf4j/jul-to-slf4j "1.7.30"]
+                 [org.slf4j/jcl-over-slf4j "1.7.30"]
+                 [org.slf4j/log4j-over-slf4j "1.7.30"]]
+  :min-lein-version "2.0.0"
+  :resource-paths ["config", "resources"]
+  :profiles {:dev {:aliases {"run-dev" ["trampoline" "run" "-m" "sample-grpc-server.server/run-dev"]}
+                   :dependencies [[io.pedestal/pedestal.service-tools "0.5.7"]]}
+             :uberjar {:aot [sample-grpc-server.server]}}
+  :main ^{:skip-aot true} sample-grpc-server.server)
